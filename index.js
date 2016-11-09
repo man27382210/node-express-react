169f7a42d5d4accf9fbcec8568884c1e06d49dd2
Rebecca <yunchu.chen@rakuten.com>
2016-11-09 11:39:49 +0800

    modified to host static html

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+app.use(express.static(__dirname));
 
 app.listen(3000, function () {
   console.log('application listening on port 3000!');
