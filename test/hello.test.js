4d124745dbff71b08e8742925290fc78298e1740
Rebecca <joanna8030@gmail.com>
2016-11-25 22:26:18 +0800

    add testing code for hello.jsx

--- test/hello.test.js
+++ test/hello.test.js
@@ -1,13 +1,17 @@
 import chai from 'chai';
 import ReactTestUtils from 'react-addons-test-utils';
 import React from 'react';
-var should = chai.should();
+import ReactDOM from 'react-dom';
+const should = chai.should();
+const expect = chai.expect;
 import HelloWorld from '../src/hello.jsx';
 
 describe('HelloWorld DOM Rendering', () => {
   it('should render into div and show hello world', () => {
 
     const helloWorld = ReactTestUtils.renderIntoDocument(<HelloWorld />);
-    
+
+    expect(ReactDOM.findDOMNode(helloWorld).tagName).to.eq('DIV');
+    expect(ReactDOM.findDOMNode(helloWorld).querySelectorAll("h1")[0].textContent).to.eq('Hello World!!!');
   })
 })
