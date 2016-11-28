033ae74cba9a364901ff6b0e6a0c5ebc14dff8b6
Rebecca <joanna8030@gmail.com>
2016-11-28 09:40:33 +0800

    fix wrong package dependencies with eslint rule

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+/* eslint import/no-extraneous-dependencies: ["error", {"devDependencies": true}] */
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
