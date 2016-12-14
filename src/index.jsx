db68b2ca073cd6bebf0f675bc03f6049ce52a9fc
Rebecca <joanna8030@gmail.com>
2016-12-14 17:46:13 +0800

    use redux thunk

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import App from './containers/container';
 import reducer from './reducers';
 
-const store = createStore(reducer);
+const store = createStore(reducer, applyMiddleware(thunk));
 
 ReactDOM.render(
   <Provider store={store}>
