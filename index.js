b0d5054ab1d369594cbad55e2489cc70d402f54f
Rebecca <joanna8030@gmail.com>
2017-01-16 18:17:25 +0800

    handle request error

--- index.js
+++ index.js
@@ -9,29 +9,29 @@ app.use(express.static(__dirname));
 
 app.get('/issues', function(req, res) {
   DB.find({ timeStamp: null }, function(err, docs) {
-    if (err) console.log(err);
+    if (err) res.json([{}]);
     res.json(docs);
   });
 });
 
-
 app.post('/issues', function(req, res) {
   const issue = new DB(req.body.issue);
   issue.save(issue, function(err, docs) {
-    if (err) console.log(err);
+    if (err) res.json({});
     res.json(docs);
   });
 });
 
 app.put('/issues/:issue_id', function(req, res) {
-  DB.findOneAndUpdate({ seq: req.params.issue_id }, req.body.issue, function() {
+  DB.findOneAndUpdate({ seq: req.params.issue_id }, req.body.issue, function(err) {
+    if (err) res.json({});
     res.json(req.body.issue);
   });
 });
 
 app.delete('/issues/:issue_id', function(req, res) {
   DB.findOneAndUpdate({ seq: req.params.issue_id }, { timeStamp: new Date() }, function(err, doc) {
-    console.log('delete: ', doc.seq);
+    if (err) res.json({});
     res.json(doc.seq);
   });
 });
